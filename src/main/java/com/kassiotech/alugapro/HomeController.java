0b23e2c00b5a04b9dfa6ae470488f5b99219e723
Kassio <kassiofelipe26@gmail.com>
2025-01-19 22:37:04 -0300

    feat: criando a rota padrao para a tela home

---
+++ src/main/java/com/kassiotech/alugapro/HomeController.java
@@ -0,0 +1,16 @@
+package com.kassiotech.alugapro;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Home - Gerenciamento de Aluguéis");
+        
+        return "home";
+    }
+}
