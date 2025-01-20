ed187f3afa6329d00ef9810314bfcf347713e130
Kassio <kassiofelipe26@gmail.com>
2025-01-19 21:37:13 -0300

    Primeiro commit - projeto inicial

---
+++ src/main/java/com/kassiotech/alugapro/AlugaProApplication.java
@@ -0,0 +1,13 @@
+package com.kassiotech.alugapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlugaProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlugaProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kassiotech/alugapro/AlugaProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kassiotech.alugapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlugaProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
